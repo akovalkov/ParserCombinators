50bd070b20a80841749c01f2663f5eb62a834ad3
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 <akovalkov@yandex.ru>
2024-03-31 11:37:00 +0300

    Добавьте файлы проекта.

---
+++ ParserCombinators.cpp
@@ -0,0 +1,45 @@
+﻿// ParserCombinations.cpp: определяет точку входа для приложения.
+//
+#include <print>
+#include <iostream>
+#include <string>
+#include "ParserCombinators.h"
+
+template<typename T> concept ParserFunction = requires(T t, const std::string_view & targetString) {
+	t(targetString);
+};
+
+
+auto make_str(const std::string_view& prefix) {
+	auto str = [&prefix](const std::string_view& targetString) {
+		if (targetString.starts_with(prefix)) {
+			return prefix;
+		}
+		throw std::runtime_error(std::format("Tried to match \"{}\", but got \"{}\"", prefix, targetString.substr(0, 10)));
+	};
+	return str;
+}
+
+template<ParserFunction Parser>
+auto run(Parser parser, const std::string_view& targetString) {
+	return parser(targetString);
+}
+
+
+void func(const std::string_view& targetString) {}
+
+
+int main()
+{
+	auto str = make_str("Hello there!");
+	try {
+		auto result = run(str, "Hello there!");
+		std::println("Result: {}", result);
+		run(str, "test");
+	}
+	catch (std::runtime_error& err) {
+		std::println(std::cerr, "Error: {}", err.what());
+	}
+
+	return 0;
+}
